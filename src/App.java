ca38c169073c93d27b6e2e85ff08ad7e0fd21458
seomin1210 <seoyun0700@daum.net>
2025-09-25 15:09:57 +0900

    9/25

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int x = 33, y = 13;
+        int big = x > y ? x : y;
+        System.out.println("Bit = " + big);
     }
 }
